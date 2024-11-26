51b6c2c68fc6bb1eb764737857cd92ef167c08db
Adam <Adam.sturesson@live.se>
2024-11-26 14:59:15 +0100

    added api-request for info and rendering the result, also added interval to update every second

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [data, setData] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get('http://127.0.0.1:5000/info')
+        .then(response => {
+          setData(response.data)
+      })
+      .catch(error => console.log(error));
+  };
+
+  fetchData();
+
+  const interval = setInterval(fetchData, 1000);
+
+  return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-      <div></div>
+     <div>
+      {data ? (
+        <div>
+          <p>Simulated time: {data.sim_time_hour}:{data.sim_time_min}</p>
+          <p>Baseload: {data.base_current_load} kWh</p>
+          <p>Battery capacity: {data.battery_capacity_kWh} kWh</p>
+          <p>Charging? {data.ev_battery_charge_start_stopp ? 'Yes' : 'No'}</p>
+        </div>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
     </>
   )
 }
