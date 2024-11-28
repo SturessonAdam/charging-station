30fe1bdc623d13dc50dc84b883634e0318cb1d61
Adam <Adam.sturesson@live.se>
2024-11-28 11:28:31 +0100

    added comments

--- src/components/BatteryHandler.tsx
+++ src/components/BatteryHandler.tsx
@@ -5,6 +5,7 @@ function BatteryHandler() {
     const [batterylife, setBatterylife] = useState(null)
     const [charge, setCharge] = useState(false);
 
+    //API anrop för att hämta batteri %
     useEffect(() => {
         const fetchData = () => {
           axios.get('http://127.0.0.1:5000/charge')
@@ -13,14 +14,12 @@ function BatteryHandler() {
           })
           .catch(error => console.log(error));
       };
-    
       fetchData();
-    
       const interval = setInterval(fetchData, 1000);
-    
       return () => clearInterval(interval);
       }, []);
 
+    //API anrop för att starta laddningen
     const startCharge = () => {
         axios.post('http://127.0.0.1:5000/charge', {
             "charging" : "on"
